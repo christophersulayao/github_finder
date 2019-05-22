7eb854ecb2e45eac26a53143865ddc2e67cc4509
christophersulayao <christoper.sulayao@gmail.com>
2019-05-22 06:44:48 -0700

    added more links

--- js/main.js
+++ js/main.js
@@ -19,9 +19,21 @@ $(document).ready(function(){
                 <div class="panel-body">
                    <div class="row">
                         <div class="col-md-3">
-                        <img src="${user.avatar_url}">
+                            <img class="thumbnail avatar" src="${user.avatar_url}">
+                            <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
                         </div>
                         <div class="col-md-9">
+                            <span class="label label-default">Public Repos: ${user.public_repos}</span>
+                            <span class="label label-primary">Public Gists: ${user.public_gists}</span>
+                            <span class="label label-success">Followers: ${user.followers}</span>
+                            <span class="label label-info">Following: ${user.following}</span>
+                            <br><br>
+                            <ul class="list-group">
+                                <li class="list-group-item">Company: ${user.company}</li>
+                                <li class="list-group-item">Website/Blog: ${user.blog}</li>
+                                <li class="list-group-item">Location: ${user.location}</li>
+                                <li class="list-group-item">Member Since: ${user.created_at}</li>
+                            </ul>
                         </div>
                    </div>
                 </div>
